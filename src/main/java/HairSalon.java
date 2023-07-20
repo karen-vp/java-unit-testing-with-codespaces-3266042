f5bcf779b07644357f993bad342968a603a2a43f
karenvp <karenvalierpalmero@gmail.com>
2023-07-20 12:39:05 -0600

    feat: HairSalon openDays and closedDays excercise

--- src/main/java/HairSalon.java
+++ src/main/java/HairSalon.java
@@ -2,23 +2,29 @@ import java.util.Arrays;
 
 public class HairSalon {
 
-    private String[] openingDays = {"Monday", "Tuesday"};
+    private String[] openingDays = { "Monday", "Tuesday" };
 
     // This method should return the openingDays array.
     public String[] getOpeningDays() {
-        return new String[]{};
+
+        return this.openingDays;
     }
 
-    // This method should return true if the weekday passed in is present in the openingDays array.
-    // If not it should return false. If the String passed in does not end in the letters "day",
-    // it should throw an IllegalArgumentException with a message explaining that only weekdays are valid.
+    // This method should return true if the weekday passed in is present in the
+    // openingDays array.
+    // If not it should return false. If the String passed in does not end in the
+    // letters "day",
+    // it should throw an IllegalArgumentException with a message explaining that
+    // only weekdays are valid.
     public boolean isOpen(String weekDay) {
+        if (!weekDay.contains("day")) {
+            throw new IllegalArgumentException("The weekday argument must end in the letters 'day'");
+        }
+        if (weekDay.equals(this.openingDays[0]) || weekDay.equals(this.openingDays[1])) {
+            return true;
+        }
+
         return false;
     }
-
-
-
-
-
 
 }
--- src/test/java/HairSalonTest.java
+++ src/test/java/HairSalonTest.java
@@ -1,3 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class HairSalonTest {
@@ -6,22 +12,33 @@ class HairSalonTest {
 
     @Test
     public void testGetOpeningDays() {
-
+        assertArrayEquals(new String[]{"Monday", "Tuesday"}, hairSalon.getOpeningDays());
     }
 
     @Test
     public void testSalonIsOpenOnMonday() {
-
+        assertTrue(hairSalon.isOpen("Monday"));
+        assertTrue(hairSalon.isOpen("Tuesday"));
     }
 
     @Test
     public void testSalonIsClosedOnWednesday() {
-
+         assertFalse(hairSalon.isOpen("Wednesday"));
+         assertFalse(hairSalon.isOpen("Sunday"));
     }
 
     @Test
     public void testExceptionIsThrownIfWeekdayDoesNotEndInDay() {
+        assertThrows(IllegalArgumentException.class, ()->{hairSalon.isOpen("hola");});
 
+        // Exception with a message
+        Exception exception = assertThrows(IllegalArgumentException.class, ()->{hairSalon.isOpen("vaca");});
+    
+        String expected = "The weekday argument must end in the letters 'day'";
+
+        String actual = exception.getMessage();
+
+        assertEquals(expected,actual);
     }
 
 }
