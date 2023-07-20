c328a8663698083fe83351b4f9c6c7f856ffde7d
karenvp <karenvalierpalmero@gmail.com>
2023-07-19 21:08:59 -0600

    feat: created @Test methods for the methods in Calculator class

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -15,4 +15,25 @@ public class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testSubstract(){
+      int expected = 2;
+      int actual = calculator.subtract(3, 1);
+
+      assertEquals(expected, actual);
+
+      assertEquals(-2, calculator.subtract(5,7));
+    }
+
+    @Test
+    public void testMultiply(){
+      assertEquals(10,calculator.multiply(5, 2));
+      assertEquals(49,calculator.multiply(7, 7), " multiply method ");
+    }
+
+    @Test
+    public void testDivide(){
+      assertEquals(5,calculator.divide(15,3));
+      assertEquals(1.2,calculator.divide(6,5), 0.0001);
+    }
 }
