7e05269c7886e80254449818b0bd968f87e3011c
karenvp <karenvalierpalmero@gmail.com>
2023-07-20 13:12:37 -0600

    feat: usamos Assumptions "assumeTrue"

--- src/test/java/HairSalonTest.java
+++ src/test/java/HairSalonTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class HairSalonTest {
 
@@ -8,7 +9,7 @@ class HairSalonTest {
 
     @Test
     public void testGetOpeningDays() {
-        String[] expected = new String[]{"Monday", "Tuesday"};
+        String[] expected = new String[] { "Monday", "Tuesday" };
 
         String[] actual = hairSalon.getOpeningDays();
 
@@ -32,6 +33,19 @@ class HairSalonTest {
         String expected = "The weekday argument must end in the letters 'day'";
 
         String actual = exception.getMessage();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testApplyDiscount() {
+        Calculator calculator = new Calculator();
+        assumeTrue(calculator.divide(hairSalon.getHaircutPrice(), 2) == 15, "La prueba se ignora porque la condición es falsa.");
+         
+
+        // Código de prueba que se ejecutará si la condición de assumeTrue es verdadera.
+        int expected = 20;
+        int actual = hairSalon.applyDiscount();
 
         assertEquals(expected, actual);
     }
